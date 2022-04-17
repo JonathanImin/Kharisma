d8a20a3cbd854ada3d05f040faca146e0071e7df
JonathanImin <103871247+JonathanImin@users.noreply.github.com>
2022-04-17 14:14:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Jonathan Hansen Imin | 52019025 | Teknik Informatika 2019\n')
+    res.send('Revivo Moreno Sabandar (52019029), Teknik Informatika 2019\n')
 })
 
 app.listen(PORT, HOST)
